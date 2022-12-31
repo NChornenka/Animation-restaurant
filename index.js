5842b73ed119e6406696536c34638a928d104c32
NChornenka <88328438+NChornenka@users.noreply.github.com>
2022-12-30 19:19:15 -0500

    deleted photos

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-gsap.to(".one", {delay: 1, x: -400, duration: 1.5, ease: "power0.2inOut", opacity: 0})
+gsap.to(".one", {delay: 1, x: -400, duration: 2, ease: "power0.2inOut", opacity: 0})
 gsap.from(".two", {delay: 1.2, x: -450, duration: 1.5, ease: "power0.2inOut", opacity:0})
 gsap.from(".header", {delay: 2.5, y: -100, duration: 2, ease: "power0.2inOut", opacity: 0})
