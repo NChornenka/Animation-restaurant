c568c5784cd91c6595ba975451e8b5456d29ee79
NChornenka <88328438+NChornenka@users.noreply.github.com>
2023-01-03 18:09:53 -0500

    added animation to the menu button

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-gsap.to(".one", {delay: 1, x: -400, duration: 2, ease: "power0.2inOut", opacity: 0})
-gsap.from(".two", {delay: 1.2, x: -450, duration: 1.5, ease: "power0.2inOut", opacity:0})
-gsap.from(".header", {delay: 2.5, y: -100, duration: 2, ease: "power0.2inOut", opacity: 0})
+gsap.to(".one", {delay: 1, x: -400, duration: 2, ease: "power2.out", opacity: 0})
+gsap.from(".two", {delay: 1.2, x: -450, duration: 1.5, ease: "power2.out", opacity:0})
+gsap.from(".header", {delay: 2.5, y: -100, duration: 2, ease: "power2.out", opacity: 0})
+gsap.from(".btn", {delay: 2.5, y: -100, duration: 2, ease: "power2.out", opacity: 0})
